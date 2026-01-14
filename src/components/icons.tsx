b7fc9f2b6cf0cac94dfd593bc57f7f8f530be1c7
Josep Esteve Arqués <joes013@vidalibarraquer.net>
2026-01-14 19:20:18 +0000

    busca una imatge a la carpeta public que es diu 12345.png i insertala co

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,11 +5,11 @@ export function Logo(props: SVGProps<HTMLDivElement>) {
   return (
     <div className="flex items-center gap-2" {...props}>
       <Image 
-        src="https://i.pravatar.cc/32?u=12345"
+        src="/12345.png"
         alt="EJA Globaltrans Logo" 
         width={32} 
         height={32} 
-        className="text-primary rounded-full"
+        className="text-primary"
       />
       <span className="font-headline font-bold text-xl text-primary hidden sm:inline-block">
         EJA Globaltrans
